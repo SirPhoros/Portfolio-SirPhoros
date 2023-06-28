c76a1aaa36e4b744974cab198933fbbaab96c0a9
SirPhoros <cgtorrubia@gmail.com>
2023-06-28 16:15:28 +0100

    populate Home page with basic information

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,18 @@
 export default function Home() {
 	return (
 		<>
-			<p>This is Home</p>
+			<h2>Welcome, my name is… </h2>
+			<h1>Cristóbal G. Torrubia</h1>
+			<h3>I am a Junior Full Stack Developer</h3>
+			<section>
+				<p>
+					I'm constantly expanding my knowledge and honing my skills in
+					JavaScript, building web-based solutions that go beyond mere
+					functionality. I strive to create meaningful applications that
+					facilitate connections between people, ultimately enriching their
+					lives with increased comfort and convenience.
+				</p>
+			</section>
 		</>
 	)
 }
