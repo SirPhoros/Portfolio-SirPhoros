c976bea92404f0ba93f1da6235ec162e9044d64b
SirPhoros <cgtorrubia@gmail.com>
2023-06-30 12:44:04 +0100

    add images to project page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,26 +2,28 @@ import { BsLinkedin, BsGithub } from 'react-icons/bs'
 
 export default function Contact() {
 	return (
-		<>
-			<h1 className="titlePage">You can find me in:</h1>
-			<section className="button">
-				<button>
-					<a
-						href="https://www.linkedin.com/in/cgtorrubia"
-						target="_blank"
-					>
-						<BsLinkedin /> @in/cgtorrubia
-					</a>
-				</button>{' '}
-				<button>
-					<a
-						href="https://github.com/SirPhoros"
-						target="_blank"
-					>
-						<BsGithub /> @SirPhoros
-					</a>
-				</button>
+		<main>
+			<section className="contact">
+				<h1 className="titlePage">You can find me in:</h1>
+				<section className="button">
+					<button>
+						<a
+							href="https://www.linkedin.com/in/cgtorrubia"
+							target="_blank"
+						>
+							<BsLinkedin /> @in/cgtorrubia
+						</a>
+					</button>{' '}
+					<button>
+						<a
+							href="https://github.com/SirPhoros"
+							target="_blank"
+						>
+							<BsGithub /> @SirPhoros
+						</a>
+					</button>
+				</section>
 			</section>
-		</>
+		</main>
 	)
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,144 +1,171 @@
+import LoveBirds from '../../public/images/project/Love Birds.svg'
+import Hive from '../../public/images/project/Hive.svg'
+import BackEnd from '../../public/images/project/BackEnd.svg'
+
 export default function Project() {
 	return (
 		<>
-			<section>
+			<main>
 				<h1 className="titlePage">Projects:</h1>
 				<p>
 					This is a collection of all the projects in which I have taken part.
 					Either because they were my own work, or because I was part of a group
 					project.{' '}
 				</p>
-			</section>
-			<section>
-				<section>
-					<h2>Love Birds:</h2>
-					<p>
-						Built as final project of the 13-week Full Stack Northcoders
-						Bootcamp, Love Birds is a fun and interactive application designed
-						to keep the spark in long distance relationships.
-					</p>
-					<br />
-					<p>
-						This project has been built in TypeScript, Firebase, React Native
-						and TailwindCSS. A different set of tools from the one the bootcamp
-						was taught in. <br />
-						Love Birds allows users to send images, videos and more to their
-						loved ones. Upon receiving their message, users must answer a quiz
-						question or play a game to 'hatch their egg' and receive their
-						prize.
-					</p>
-					<section className="projectButton">
-						<button>
-							<a
-								href="https://github.com/SirPhoros/love-birds"
-								target="_blank"
-							>
-								Code
-							</a>
-						</button>{' '}
-						<button>
-							<a
-								href=""
-								target="_blank"
-							>
-								Live Demo
-							</a>
-						</button>
+				<section className="project">
+					<section className="description">
+						<h2>Love Birds:</h2>
+						<p>
+							Built as final project of the 13-week Full Stack Northcoders
+							Bootcamp, Love Birds is a fun and interactive application designed
+							to keep the spark in long distance relationships.
+						</p>
+						<br />
+						<p>
+							This project has been built in TypeScript, Firebase, React Native
+							and TailwindCSS. A different set of tools from the one the
+							bootcamp was taught in. <br />
+							Love Birds allows users to send images, videos and more to their
+							loved ones. Upon receiving their message, users must answer a quiz
+							question or play a game to 'hatch their egg' and receive their
+							prize.
+						</p>
+						<section className="projectButton">
+							<button>
+								<a
+									href="https://github.com/SirPhoros/love-birds"
+									target="_blank"
+								>
+									Code
+								</a>
+							</button>{' '}
+							<button>
+								<a
+									href="https://northcoders.com/projects/june-2023/love-birds"
+									target="_blank"
+								>
+									Presentation
+								</a>
+							</button>
+						</section>
+					</section>
+					<section className="project_img">
+						<img
+							src={LoveBirds}
+							alt="Love birds app screenshots"
+						/>
 					</section>
 				</section>
-				<section>
-					<h2>The Hive:</h2>
-					<p>
-						A mock forum inspired by Reddit, built as part of the Front End
-						project phase for the Northcoders Bootcamp. articles and comments.
-					</p>
-					<br />
-					<p>
-						The Hive is built on the API fabricated for the Back End project,
-						NC_News API (see below), which is used to request said information
-						from the different databases built on Express.
+				<section className="project">
+					<section className="description">
+						<h2>The Hive:</h2>
+						<p>
+							A mock forum inspired by Reddit, built as part of the Front End
+							project phase for the Northcoders Bootcamp. articles and comments.
+						</p>
 						<br />
-						The user will have the choice to filter the articles based on the
-						topic, but also they will have the opportunity to filter by the
-						amount of comments that the articles have, when it was published
-						among other options. Finally, they will be able to comment in the
-						articles, and vote the articles that they (dis)like the most.
-					</p>
-					<p>
-						Tools: This project has been built in JavaScript, NodeJS, React,
-						Vite, and CSS.
-					</p>
-					<section className="projectButton">
-						<button>
-							<a
-								href="https://github.com/SirPhoros/CTorrubia_nc-news_FE"
-								target="_blank"
-							>
-								Code
-							</a>
-						</button>{' '}
-						<button>
-							<a
-								href="https://thehive-news.netlify.app/articles"
-								target="_blank"
-							>
-								Live Demo
-							</a>
-						</button>
+						<p>
+							The Hive is built on the API fabricated for the Back End project,
+							NC_News API (see below), which is used to request said information
+							from the different databases built on Express.
+							<br />
+							The user will have the choice to filter the articles based on the
+							topic, but also they will have the opportunity to filter by the
+							amount of comments that the articles have, when it was published
+							among other options. Finally, they will be able to comment in the
+							articles, and vote the articles that they (dis)like the most.
+						</p>
+						<p>
+							Tools: This project has been built in JavaScript, NodeJS, React,
+							Vite, and CSS.
+						</p>
+						<section className="projectButton">
+							<button>
+								<a
+									href="https://github.com/SirPhoros/CTorrubia_nc-news_FE"
+									target="_blank"
+								>
+									Code
+								</a>
+							</button>{' '}
+							<button>
+								<a
+									href="https://thehive-news.netlify.app/articles"
+									target="_blank"
+								>
+									Live Demo
+								</a>
+							</button>
+						</section>
+					</section>
+					<section className="project_img">
+						<img
+							src={Hive}
+							alt="The Hive screenshots"
+						/>
 					</section>
 				</section>
-				<section>
-					<h2>NC_News API:</h2>
-					<p>
-						An API built with PostgreSQL, Express JS and Node JS, as part of the
-						BE project phase of Northcoders bootcamp. This API functions as
-						database to later on been used in "The Hive" (see above). There is a
-						list of all the available endpoints
-					</p>
-					<br />
-					<p>
-						NC_NEWS API is built with the purpose of populating the Front End
-						project "The Hive", as said project will render its content by calls
-						into the API.
+				<section className="project">
+					<section className="description">
+						<h2>NC_News API:</h2>
+						<p>
+							An API built with PostgreSQL, Express JS and Node JS, as part of
+							the BE project phase of Northcoders bootcamp. This API functions
+							as database to later on been used in "The Hive" (see above). There
+							is a list of all the available endpoints
+						</p>
 						<br />
-						The API has been built using Express and PostgresSQL, with a
-						pre-made set of data provided by the course for the purpose of
-						having some initial data to fill our database.
-						<br />
-						Additionally, the process has been throughfully tested using the TDD
-						technique and JEST, making sure that the project is bulletproof to
-						SQL injections, but also to make sure that every endpoint returns
-						the demanded information, without mutating the data provided.
-						<br />
-						On top of that testing the endpoints using JEST and TDD, Error
-						Handling procedures have been put into place, to stop the user to
-						have access to inexistent data, while offering useful and clear
-						feedback.
-					</p>
-					<p>
-						Tools: This project has been built in JavaScript, NodeJS, Express,
-						PostgresSQL, and JEST.
-					</p>
-					<section className="projectButton">
-						<button>
-							<a
-								href="https://github.com/SirPhoros/CTorrubia-News-Solo_Project-NC"
-								target="_blank"
-							>
-								Code
-							</a>
-						</button>{' '}
-						<button>
-							<a
-								href="https://nc-news-soloproject-be.onrender.com/"
-								target="_blank"
-							>
-								Live Demo
-							</a>
-						</button>
+						<p>
+							NC_NEWS API is built with the purpose of populating the Front End
+							project "The Hive", as said project will render its content by
+							calls into the API.
+							<br />
+							The API has been built using Express and PostgresSQL, with a
+							pre-made set of data provided by the course for the purpose of
+							having some initial data to fill our database.
+							<br />
+							Additionally, the process has been throughfully tested using the
+							TDD technique and JEST, making sure that the project is
+							bulletproof to SQL injections, but also to make sure that every
+							endpoint returns the demanded information, without mutating the
+							data provided.
+							<br />
+							On top of that testing the endpoints using JEST and TDD, Error
+							Handling procedures have been put into place, to stop the user to
+							have access to inexistent data, while offering useful and clear
+							feedback.
+						</p>
+						<p>
+							Tools: This project has been built in JavaScript, NodeJS, Express,
+							PostgresSQL, and JEST.
+						</p>
+						<section className="projectButton">
+							<button>
+								<a
+									href="https://github.com/SirPhoros/CTorrubia-News-Solo_Project-NC"
+									target="_blank"
+								>
+									Code
+								</a>
+							</button>{' '}
+							<button>
+								<a
+									href="https://nc-news-soloproject-be.onrender.com/"
+									target="_blank"
+								>
+									Live Demo
+								</a>
+							</button>
+						</section>
+					</section>
+					<section className="project_img">
+						<img
+							src={BackEnd}
+							alt="API screenshots"
+						/>
 					</section>
 				</section>
-			</section>
+			</main>
 		</>
 	)
 }
